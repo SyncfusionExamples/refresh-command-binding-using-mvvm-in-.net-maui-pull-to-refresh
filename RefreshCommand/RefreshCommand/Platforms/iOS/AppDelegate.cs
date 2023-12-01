4c1679286a460586b3d83389920bcff8ca45625d
DiwakarVenkatesan <diwakar.venkatesan@syncfusion.com>
2023-12-01 15:19:27 +0530

    Added sample and updated Readme file

---
+++ RefreshCommand/RefreshCommand/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RefreshCommand
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
